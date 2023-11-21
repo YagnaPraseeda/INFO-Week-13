03064af12c8a244b4bea4e83fe9eb10c08f1843b
YagnaPraseeda <151584865+YagnaPraseeda@users.noreply.github.com>
2023-11-20 19:18:03 -0500

    Update lab_13_yagna_praseeda_atmuri_app.py

--- lab_13_yagna_praseeda_atmuri_app.py
+++ lab_13_yagna_praseeda_atmuri_app.py
@@ -3,27 +3,21 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import streamlit as st
 
-# Load data
 file_path = 'dataset.csv'
 df = pd.read_csv(file_path)
 
-# Load data
-#df = pd.read_csv('C:/Users/prase/OneDrive/Documents/INFORMATICS/Project_proposal/dataset.csv')
-
 def plot_quantity_distribution(sort_by):
-    # Group the data by 'Country' and calculate the average quantity for each country
+    
     avg_quantity_by_country = df.groupby('Country')['Quantity'].mean().sort_values(ascending=False).reset_index()
 
-    # Sort the data based on the selected sorting option
+   
     if sort_by == 'Ascending':
         avg_quantity_by_country.sort_values('Quantity', inplace=True)
     elif sort_by == 'Descending':
         avg_quantity_by_country.sort_values('Quantity', ascending=False, inplace=True)
 
-    # Set a creative color palette
     palette = sns.color_palette("pastel")
 
-    # Plotting the bar chart with seaborn
     plt.figure(figsize=(14, 8))
     sns.barplot(x='Country', y='Quantity', data=avg_quantity_by_country, palette=palette)
     plt.title('Average Quantity of Items Purchased by Country', fontsize=16)
@@ -32,10 +26,8 @@ def plot_quantity_distribution(sort_by):
     plt.xticks(rotation=45, ha='right')  # Rotate x-axis labels for better readability
     plt.tight_layout()
 
-    # Display the plot using Streamlit
     st.pyplot(plt)
 
-# Streamlit app
 def main():
     st.title('Average Quantity Distribution by Country')
     st.sidebar.header('Sort Options')
